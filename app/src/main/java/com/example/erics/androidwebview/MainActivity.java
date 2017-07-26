6828be6c4036cceb7a8b4fe4a291ef77605ba7be
Eric Humberto Cypriano de Souza <erics@cit>
2017-07-25 21:17:50 -0300

    Refresh button

--- app/src/main/java/com/example/erics/androidwebview/MainActivity.java
+++ app/src/main/java/com/example/erics/androidwebview/MainActivity.java
@@ -2,19 +2,34 @@ package com.example.erics.androidwebview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import static com.example.erics.androidwebview.R.id.webview;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView = (WebView)findViewById(R.id.webview);
+        this.webView = (WebView) findViewById(webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("http://10.0.2.2:4200/");
+        webView.loadUrl("http://10.0.2.2:3000/");
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                webView.reload();
+            }
+        });
     }
 }
